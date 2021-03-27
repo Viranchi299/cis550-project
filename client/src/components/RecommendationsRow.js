7291dec5faa02c1b005ffa77dd37c29af043bf97
joshua-chopra <jchopra@seas.upenn.edu>
2021-03-26 23:25:01 -0400

    pass props in to func.

--- client/src/components/RecommendationsRow.js
+++ client/src/components/RecommendationsRow.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const RecommendationsRow = () => {
+const RecommendationsRow = (props) => {
 
 	return (
 		<div className="movieResults">
