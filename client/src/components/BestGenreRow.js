0165e03fe625142e312f0f8cf263b9248fe9119d
joshua-chopra <jchopra@seas.upenn.edu>
2021-03-28 18:26:08 -0400

    styling for returning query results loadHomes()

--- client/src/components/BestGenreRow.js
+++ client/src/components/BestGenreRow.js
@@ -4,8 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const BestGenreRow = (props) => {
 		return (
 			<div className="movieResults">
-				<div className="genre">{props.genre}</div>
-				<div className="rating">{props.rating}</div>
+				{/* use default class names from css styling to get them to align for now, will update! */}
+				<div className="genre">{props.City}</div>
+				<div className="rating">{props.State}</div>
+				<div className="rating">{props.MinHVP}</div>
+				<div className="rating">{props.MaxHVP}</div>
+				<div className="rating">{props.AvgHVP}</div>
 			</div>
 		);
 }
