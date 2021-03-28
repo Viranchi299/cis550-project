4518fe8dc1b1954a9c2d6fd51719987cfe3c799f
joshua-chopra <jchopra@seas.upenn.edu>
2021-03-28 17:39:31 -0400

    update page for state/city specific queries

---
+++ client/src/components/Analyze.js
@@ -0,0 +1,169 @@
+import React, { useState, useEffect } from 'react';
+import PageNavbar from './PageNavbar';
+import BestGenreRow from './BestGenreRow';
+import '../style/BestGenres.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Analyze = (props) => {
+	const [selectedState, setSelectedState] = useState("");
+	const [statesList, setStatesList] = useState([]);
+	const [selectedCity, setSelectedCity] = useState("");
+	const [cityList, setCitiesList] = useState([]);
+	const [houses, setHouses] = useState([]);
+
+	const useMountEffect = (func) => useEffect(func, []);
+
+	// call at very beginning so user can see all the states 
+	useMountEffect(loadStates);
+
+	// set state for selected "state name e.g., FL" 
+	const handleChangeStateName = (event) => { 
+		setSelectedState(event.target.value);
+	}
+
+	// set the city for the relevant state, e.g., [Orlando] after FL was selected 
+	const handleChangeCityName = (event) => {
+		setSelectedCity(event.target.value);
+		console.log(selectedCity);
+	}
+
+	function loadStates() {
+		fetch("http://localhost:8081/homerent/rentpricestate",
+			{
+				method: 'GET' // The type of HTTP request.
+			}).then(res => {
+				// Convert the response data to a JSON.
+				console.log('something?');
+				return res.json();
+			}, err => {
+				// Print the error if there is one.
+				console.log(err);
+			}).then((stateNames) => {
+				console.log(stateNames);
+				if (!stateNames) return;
+				let stateNameDivs = stateNames.map((state, i) =>
+					<option value={state.State}>{state.State}</option>
+				);
+				setStatesList(stateNameDivs);
+			}, (err) => {
+				// Print the error if there is one.
+				console.log(err);
+			});
+	}
+
+	function loadCities() {
+		console.log("Calling loadCities with:" + selectedState);
+		fetch(`http://localhost:8081/homerent/getcitieshousing/${selectedState}`,
+			{
+				method: 'GET' // The type of HTTP request.
+			}).then(res => {
+				// Convert the response data to a JSON.
+				console.log('something?');
+				return res.json();
+			}, err => {
+				// Print the error if there is one.
+				console.log(err);
+			}).then((cityList) => {
+				console.log(cityList);
+				if (!cityList) return;
+				let cityDivs = cityList.map((city, i) =>
+					<option value={city.City}>{city.City}</option>
+				);
+				console.log(cityDivs);
+				//Set the state of the genres list to the value returned by the HTTP response from the server.
+				setCitiesList(cityDivs);
+			}, (err) => {
+				// Print the error if there is one.
+				console.log(err);
+			});
+	}
+
+	function submitCityState() {
+		fetch(`http://localhost:8081/homerent/${selectedState}&${selectedCity}`,
+			{
+				method: 'GET' // The type of HTTP request.
+			}).then(res => {
+				// Convert the response data to a JSON.
+				console.log('something?');
+				return res.json();
+			}, err => {
+				// Print the error if there is one.
+				console.log(err);
+			}).then((cityList) => {
+				console.log(cityList);
+				if (!cityList) return;
+				let cityDivs = cityList.map((city, i) =>
+					<option value={city.City}>{city.City}</option>
+				);
+				console.log(cityDivs);
+				//Set the state of the genres list to the value returned by the HTTP response from the server.
+				setCitiesList(cityDivs);
+			}, (err) => {
+				// Print the error if there is one.
+				console.log(err);
+			});
+	}
+
+
+	const StatesDropDown = () => {
+		return (
+			<div className="years-container">
+				<div className="dropdown-container">
+					<select value={selectedState} onChange={handleChangeStateName} className="dropdown" id="decadesDropdown">
+						<option select value> -- select a state -- </option>
+						{statesList}
+					</select>
+					<button className="submit-btn" id="decadesSubmitBtn" onClick={loadCities}>Submit</button>
+				</div>
+			</div>
+		);
+	}
+
+	const CityDropDown = () => {
+		return (
+			<div className="citiesContainer">
+				<div className="dropdown-container">
+					<select value={selectedCity} onChange={handleChangeCityName} className="dropdown" id="cityDropdown">
+						<option select value> -- select a city -- </option>
+						{cityList}
+					</select>
+					<button className="submit-btn" id="decadesSubmitBtn" onClick={submitCityState}>Submit</button>
+				</div>
+			</div>
+		);
+	}
+
+	const HousesContainer = () => {
+		return (
+			<div className="movies-container">
+				<div className="movie">
+					<div className="header"><strong>Genre</strong></div>
+					<div className="header"><strong>Average Rating</strong></div>
+				</div>
+				<div className="movies-container" id="results">
+					{houses}
+				</div>
+			</div>
+		);
+	}
+
+
+	return (
+		<div className="BestGenres">
+			<PageNavbar active="analyze" />
+			<div className="container bestgenres-container">
+				<div className="jumbotron">
+					<div className="h5">States</div>
+					<StatesDropDown />
+					<br/>
+					<div className="h5">Cities</div>
+					<CityDropDown />
+				</div>
+				<div className="jumbotron">
+					<HousesContainer />
+				</div>
+			</div>
+		</div>
+	);
+}
+export default Analyze;
