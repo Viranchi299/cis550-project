247002c4b8a9e93f888e77df4df76a1c5ebf2c7f
joshua-chopra <jchopra@seas.upenn.edu>
2021-03-29 21:54:28 -0400

    updates to display, change header

--- client/src/components/PageNavbar.js
+++ client/src/components/PageNavbar.js
@@ -1,13 +1,13 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const PageNavBar = (props) => {
 	const [navDivs, setNavDivs] = useState(true);
-	
+
 
 	useEffect(() => {
-	// list of pages for navbar, change when needed. 
-	const pageList = ['dashboard', 'recommendations', 'analyze','Posters'];
+		// list of pages for navbar, change when needed. 
+		const pageList = ['dashboard', 'recommendations', 'analyze', 'Posters'];
 		let navbarDivs = pageList.map((page, i) => {
 			if (props.active === page) {
 				return <a className="nav-item nav-link active" key={i} href={"/" + page}>{page.charAt(0).toUpperCase() + page.substring(1, page.length)}</a>
@@ -17,20 +17,22 @@ const PageNavBar = (props) => {
 			}
 		})
 		setNavDivs(navbarDivs);
-		// pass as "second arg" to see if component updated, i.e., which page we're actively on to set it to active page. 
+		// pass as "second arg" to see if component updated, i.e., if props.active changed since we check 
+		// which page we're actively on to set it to active page and if so, then call useEffect().
+		// otherwise (if we haven't switched pages), no need to call this and set state to cause a re-render.  
 	}, [props.active]);
 
-		return (
-			<div className="PageNavbar">
-				<nav className="navbar navbar-expand-lg navbar-light bg-light">
-			      <span className="navbar-brand center">CIS550 HW2</span>
-			      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-			        <div className="navbar-nav">
-			        {navDivs}
-			        </div>
-			      </div>
-			    </nav>
-			</div>
-        );
+	return (
+		<div className="PageNavbar">
+			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+				<span className="navbar-brand center">CIS550 Group Project</span>
+				<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+					<div className="navbar-nav">
+						{navDivs}
+					</div>
+				</div>
+			</nav>
+		</div>
+	);
 }
 export default PageNavBar;
