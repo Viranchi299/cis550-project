beb5d4f1fa0cfaa776ea863d8ce46275c861e252
joshua-chopra <jchopra@seas.upenn.edu>
2021-04-13 16:01:11 -0400

    add dbconfig

---
+++ server/db-config.js
@@ -0,0 +1,8 @@
+// Private. Will not be included in submission
+module.exports = {
+  host: "housing-cis550-proj.cgkmdltes6oq.us-east-1.rds.amazonaws.com",
+  port: "3306",
+  user: "admin",
+  password: "Penn!!098",
+  database: "housing", 
+};
