b86927bd0d006fe21a44f31303d106af3c92fbe6
Viranchi299 <vpatel29@gmail.com>
2021-04-14 14:42:06 +1200

    added home, rental, salaries toggle to dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import '../style/Dashboard.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import PageNavbar from './PageNavbar';
-import DashboardStateRow from './DashboardStateRow';
+import React, { useEffect, useState } from "react";
+import "../style/Dashboard.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, makeStyles, withStyles, Typography } from "@material-ui/core";
+import PageNavbar from "./PageNavbar";
+import DashboardStateRow from "./DashboardStateRow";
 
 import ReactTooltip from "react-tooltip";
 import MapChart from "./MapChart";
 
-// library to convert state abbreviations, e.g. AL to Alabama. 
-const allStates = require('us-state-converter');
-
+// library to convert state abbreviations, e.g. AL to Alabama.
+const allStates = require("us-state-converter");
 
 const Dashboard = () => {
   const [stateRows, setStateRows] = useState([]);
@@ -18,68 +18,234 @@ const Dashboard = () => {
 
   const [content, setContent] = useState("");
 
-
   // custom hook, we only need useEffect to run this function once similar to componentDidMount
   const useMountEffect = (func) => useEffect(func, []);
 
-  useMountEffect(getAllStatesData);
+  let choice = "salary";
 
-  function getAllStatesData() {
-    fetch("http://localhost:8081/home/homevaluestate",
-      {
-        method: 'GET' // The type of HTTP request.
-      }).then(res => {
-        // Convert the response data to a JSON.
-        return res.json();
-      }, err => {
-        // Print the error if there is one.
-        console.log(err);
-      }).then((stateList) => {
-        console.log(stateList);
-        if (!stateList) return;
-        // map array of objects to object of objects so we can index by state initial e.g., "AL"... 
-        const newObj = Object.assign({}, ...(stateList.map(item => ({ [item.State]: {MinHVP: item.MinHVP, MaxHVP: item.MaxHVP, AvgHVP: item.AvgHVP} }) )));
-        setStatesQueryRes(newObj);
-        let states = stateList.map((state, i) =>
-          <DashboardStateRow state={state.State === "OR" ? "Oregon" : allStates.fullName(state.State)} minHVP={state.MinHVP.toLocaleString(undefined)} 
-          MaxHVP={state.MaxHVP.toLocaleString(undefined)}
-          AvgHVP={state.AvgHVP.toLocaleString(undefined)} />
-        );
-        setStateRows(states);
-      }, (err) => {
-        // Print the error if there is one.
-        console.log(err);
-      });
-    }
+  useMountEffect(getAllStatesHousePriceData);
+  useMountEffect(getAllStatesSalaryData);
 
-  const stateTableData = ( 
+  //"http://localhost:8081/home/homevaluestate"
+
+  function getAllStatesHousePriceData() {
+    console.log("Hoice");
+    console.log(choice);
+    fetch("http://localhost:8081/home/homevaluestate", {
+      method: "GET", // The type of HTTP request.
+    })
+      .then(
+        (res) => {
+          // Convert the response data to a JSON.
+          return res.json();
+        },
+        (err) => {
+          // Print the error if there is one.
+          console.log(err);
+        }
+      )
+      .then(
+        (stateList) => {
+          console.log("StateList: ");
+          console.log(stateList);
+          if (!stateList) return;
+          // map array of objects to object of objects so we can index by state initial e.g., "AL"...
+          const newObj = Object.assign(
+            {},
+            ...stateList.map((item) => ({
+              [item.State]: {
+                Min: item.MinHVP,
+                Max: item.MaxHVP,
+                Avg: item.AvgHVP,
+              },
+            }))
+          );
+          setStatesQueryRes(newObj);
+          let states = stateList.map((state, i) => (
+            <DashboardStateRow
+              state={
+                state.State === "OR"
+                  ? "Oregon"
+                  : allStates.fullName(state.State)
+              }
+              minHVP={state.MinHVP.toLocaleString(undefined)}
+              MaxHVP={state.MaxHVP.toLocaleString(undefined)}
+              AvgHVP={state.AvgHVP.toLocaleString(undefined)}
+            />
+          ));
+          setStateRows(states);
+        },
+        (err) => {
+          // Print the error if there is one.
+          console.log(err);
+        }
+      );
+  }
+
+  function getAllStatesRentalData() {
+    console.log("Hoice");
+    console.log(choice);
+    fetch("http://localhost:8081/rent/rentpricestate", {
+      method: "GET", // The type of HTTP request.
+    })
+      .then(
+        (res) => {
+          // Convert the response data to a JSON.
+          return res.json();
+        },
+        (err) => {
+          // Print the error if there is one.
+          console.log(err);
+        }
+      )
+      .then(
+        (stateList) => {
+          console.log("StateList: ");
+          console.log(stateList);
+          if (!stateList) return;
+          // map array of objects to object of objects so we can index by state initial e.g., "AL"...
+          const newObj = Object.assign(
+            {},
+            ...stateList.map((item) => ({
+              [item.State]: {
+                Min: item.MinRent,
+                Max: item.MaxRent,
+                Avg: item.AvgRent,
+              },
+            }))
+          );
+          setStatesQueryRes(newObj);
+          let states = stateList.map((state, i) => (
+            <DashboardStateRow
+              state={
+                state.State === "OR"
+                  ? "Oregon"
+                  : allStates.fullName(state.State)
+              }
+              minHVP={state.MinRent.toLocaleString(undefined)}
+              MaxHVP={state.MaxRent.toLocaleString(undefined)}
+              AvgHVP={state.AvgRent.toLocaleString(undefined)}
+            />
+          ));
+          setStateRows(states);
+        },
+        (err) => {
+          // Print the error if there is one.
+          console.log(err);
+        }
+      );
+  }
+
+  function getAllStatesSalaryData() {
+    fetch("http://localhost:8081/salary/salarystate", {
+      method: "GET", // The type of HTTP request.
+    })
+      .then(
+        (res) => {
+          // Convert the response data to a JSON.
+          return res.json();
+        },
+        (err) => {
+          // Print the error if there is one.
+          console.log(err);
+        }
+      )
+      .then(
+        (stateList) => {
+          console.log("Salary List: ");
+          console.log(stateList);
+          if (!stateList) return;
+          // map array of objects to object of objects so we can index by state initial e.g., "AL"...
+          const newObj = Object.assign(
+            {},
+            ...stateList.map((item) => ({
+              [item.State]: {
+                Min: item.MinSalary,
+                Max: item.MaxSalary,
+                Avg: item.AvgSalary,
+              },
+            }))
+          );
+          setStatesQueryRes(newObj);
+
+          console.log("New obj:");
+          console.log(newObj);
+
+          let states = stateList.map((state, i) => (
+            <DashboardStateRow
+              state={
+                state.State === "OR"
+                  ? "Oregon"
+                  : allStates.fullName(state.State)
+              }
+              minSalary={state.MinSalary.toLocaleString(undefined)}
+              MaxSalary={state.MaxSalary.toLocaleString(undefined)}
+              AvgSalary={state.AvgSalary.toLocaleString(undefined)}
+            />
+          ));
+          setStateRows(states);
+        },
+        (err) => {
+          // Print the error if there is one.
+          console.log(err);
+        }
+      );
+  }
+
+  const stateTableData = (
     <table className="table table-hover table-striped">
-          <thead className="table-dark">
-            <tr>
-              <th scope="col">State Name</th>
-              <th scope="col">Minimum Home Value Price</th>
-              <th scope="col">Max Home Value Price</th>
-              <th scope="col">Average Home Value Price</th>
-            </tr>
-          </thead>
-          <tbody>
-          {stateRows}
-          </tbody>
-    </table>);
+      <thead className="table-dark">
+        <tr>
+          <th scope="col">State Name</th>
+          <th scope="col">Minimum Home Value Price</th>
+          <th scope="col">Max Home Value Price</th>
+          <th scope="col">Average Home Value Price</th>
+        </tr>
+      </thead>
+      <tbody>{stateRows}</tbody>
+    </table>
+  );
 
   return (
     <div className="Dashboard">
       <PageNavbar active="dashboard" />
       <br></br>
       {/* <div className="jumbotron"> */}
-        {/* {stateTableData} */}
+      {/* {stateTableData} */}
       {/* </div> */}
       <div>
-        <MapChart setTooltipContent={setContent} statesQueryRes={statesQueryRes} />
+        <Button
+          className="choice"
+          variant="contained"
+          onClick={() => getAllStatesHousePriceData()}
+        >
+          Home Values
+        </Button>
+        <Button
+          className="choice"
+          variant="contained"
+          onClick={() => getAllStatesRentalData()}
+        >
+          Rental Values
+        </Button>
+        <Button
+          className="choice"
+          variant="contained"
+          onClick={() => getAllStatesSalaryData()}
+        >
+          Salaries
+        </Button>
+      </div>
+
+      <div>
+        <MapChart
+          setTooltipContent={setContent}
+          statesQueryRes={statesQueryRes}
+        />
         <ReactTooltip html={true}> {content} </ReactTooltip>
       </div>
     </div>
   );
-}
+};
 
 export default Dashboard;
--- client/src/components/MapChart.js
+++ client/src/components/MapChart.js
@@ -4,115 +4,122 @@ import {
   Geographies,
   Marker,
   Geography,
-  Annotation
+  Annotation,
 } from "react-simple-maps";
-import { geoCentroid } from 'd3-geo';
-import allStates from './allStates.json';
+import { geoCentroid } from "d3-geo";
+import allStates from "./allStates.json";
 import ReactTooltip from "react-tooltip";
 
-const geoUrl =
-'https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json';
+const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";
 
 const offsets = {
-    VT: [50, -8],
-    NH: [34, 2],
-    MA: [30, -1],
-    RI: [28, 2],
-    CT: [35, 10],
-    NJ: [34, 1],
-    DE: [33, 0],
-    MD: [47, 10],
-    DC: [49, 21],
-  };
+  VT: [50, -8],
+  NH: [34, 2],
+  MA: [30, -1],
+  RI: [28, 2],
+  CT: [35, 10],
+  NJ: [34, 1],
+  DE: [33, 0],
+  MD: [47, 10],
+  DC: [49, 21],
+};
 
-  const rounded = num => {
-    if (num > 1000000000) {
-      return Math.round(num / 100000000) / 10 + "Bn";
-    } else if (num > 1000000) {
-      return Math.round(num / 100000) / 10 + "M";
-    } else {
-      return Math.round(num / 100) / 10 + "K";
-    }
-  };
-  
-  
+const rounded = (num) => {
+  if (num > 1000000000) {
+    return Math.round(num / 100000000) / 10 + "Bn";
+  } else if (num > 1000000) {
+    return Math.round(num / 100000) / 10 + "M";
+  } else {
+    return Math.round(num / 100) / 10 + "K";
+  }
+};
 
+//stateQueryRes = states query results (make generic)
 const MapChart = ({ statesQueryRes, setTooltipContent }) => {
-    
-    const handleMouseEnter = (stateAbbreviation) => {
-        const text = `Min Housing Value is: $${rounded(statesQueryRes[stateAbbreviation].MinHVP)} <br/>
-        Avg Housing Value is: $${rounded(statesQueryRes[stateAbbreviation].AvgHVP)}<br/>
-        Max Housing Value is: $${rounded(statesQueryRes[stateAbbreviation].MaxHVP)}`
-        setTooltipContent(text);
-    }
+  //sets tooltip
 
-    return (
-        <ComposableMap projection="geoAlbersUsa">
-          <Geographies data-tip="" geography={geoUrl}>
-            {({ geographies }) => (
-              <>
-                {geographies.map((geo) => {
-                //   const fillColor = getStateWinnerColor(geo.id);
-                  return (
-                    <Geography
-                      key={geo.rsmKey}
-                      stroke="#FFF"
-                      geography={geo}
-                      fill="#DDD"
-                      onMouseEnter={() => {
-                        // const { NAME, POP_EST } = geo.properties;
-                        // setTooltipContent(`${NAME} — `);
-                        const stateAbbrev = allStates.find((s) => s.val === geo.id);
-                        // console.log("Abbrevation is: " + stateAbbrev.id);
-                        handleMouseEnter(stateAbbrev.id);
-                      }}
-                      onMouseLeave={() => {
-                        setTooltipContent(null);
-                      }}
+  console.log("StatesQueryRes:");
+  console.log(statesQueryRes);
 
-                    />
-                  );
-                })}
-                {geographies.map((geo) => {
-                  const centroid = geoCentroid(geo);
-                  const cur = allStates.find((s) => s.val === geo.id);
-                  // cur.id is state name. 
-                //   console.log(cur.id);
-                //   const fillColor = getStateWinnerColor(geo.id);
-                  return (
-                    <g key={geo.rsmKey + '-name'}>
-                      {cur &&
-                        centroid[0] > -160 &&
-                        centroid[0] < -67 &&
-                        // if state abbrev not in offset list put abbrev in state else annotate outside. 
-                        (Object.keys(offsets).indexOf(cur.id) === -1 ? (
-                          <Marker coordinates={centroid}>
-                            <text
-                              y="2"
-                              fontSize={14}
-                              textAnchor="middle"
-                            >
-                              {cur.id}
-                            </text>
-                          </Marker>
-                        ) : (
-                          <Annotation
-                            subject={centroid}
-                            dx={offsets[cur.id][0]}
-                            dy={offsets[cur.id][1]}
-                          >
-                            <text x={4} fontSize={14} alignmentBaseline="middle" fill="#F10">
-                              {cur.id}
-                            </text>
-                          </Annotation>
-                        ))}
-                    </g>
-                  );
-                })}
-              </>
-            )}
-          </Geographies>
-        </ComposableMap>
-      );
-    };
+  const handleMouseEnter = (stateAbbreviation) => {
+    let textV = "Salary";
+    const text = `Min ${textV}: $${rounded(
+      statesQueryRes[stateAbbreviation].Min
+    )} <br/>
+        Avg Housing Value: $${rounded(
+          statesQueryRes[stateAbbreviation].Avg
+        )}<br/>
+        Max Housing Value: $${rounded(statesQueryRes[stateAbbreviation].Max)}`;
+    setTooltipContent(text);
+  };
+
+  return (
+    <ComposableMap projection="geoAlbersUsa">
+      <Geographies data-tip="" geography={geoUrl}>
+        {({ geographies }) => (
+          <>
+            {geographies.map((geo) => {
+              //   const fillColor = getStateWinnerColor(geo.id);
+              return (
+                <Geography
+                  key={geo.rsmKey}
+                  stroke="#FFF"
+                  geography={geo}
+                  fill="#DDD"
+                  onMouseEnter={() => {
+                    // const { NAME, POP_EST } = geo.properties;
+                    // setTooltipContent(`${NAME} — `);
+                    const stateAbbrev = allStates.find((s) => s.val === geo.id);
+                    // console.log("Abbrevation is: " + stateAbbrev.id);
+                    handleMouseEnter(stateAbbrev.id);
+                  }}
+                  onMouseLeave={() => {
+                    setTooltipContent(null);
+                  }}
+                />
+              );
+            })}
+            {geographies.map((geo) => {
+              const centroid = geoCentroid(geo);
+              const cur = allStates.find((s) => s.val === geo.id);
+              // cur.id is state name.
+              //   console.log(cur.id);
+              //   const fillColor = getStateWinnerColor(geo.id);
+              return (
+                <g key={geo.rsmKey + "-name"}>
+                  {cur &&
+                    centroid[0] > -160 &&
+                    centroid[0] < -67 &&
+                    // if state abbrev not in offset list put abbrev in state else annotate outside.
+                    (Object.keys(offsets).indexOf(cur.id) === -1 ? (
+                      <Marker coordinates={centroid}>
+                        <text y="2" fontSize={14} textAnchor="middle">
+                          {cur.id}
+                        </text>
+                      </Marker>
+                    ) : (
+                      <Annotation
+                        subject={centroid}
+                        dx={offsets[cur.id][0]}
+                        dy={offsets[cur.id][1]}
+                      >
+                        <text
+                          x={4}
+                          fontSize={14}
+                          alignmentBaseline="middle"
+                          fill="#F10"
+                        >
+                          {cur.id}
+                        </text>
+                      </Annotation>
+                    ))}
+                </g>
+              );
+            })}
+          </>
+        )}
+      </Geographies>
+    </ComposableMap>
+  );
+};
 export default memo(MapChart);
